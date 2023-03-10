9b3f5c078a6262584fc632935d8f403dc85b9940
purplesprinklesdev <bombermatt360@gmail.com>
2023-03-10 17:16:39 -0500

    Save files to OS specific config directory

---
+++ src/main/java/personal/mstall/main/OSUtils.java
@@ -0,0 +1,38 @@
+package personal.mstall.main;
+
+public final class OSUtils {
+    public enum OS {
+        WINDOWS,
+        OSX,
+        LINUX
+    }
+
+    private static OS os = null;
+    private static String userHome = null;
+
+    public static OS getOS() {
+        if (os == null) 
+            os = findOS();
+        return os;
+    }
+
+    public static String getUserHome() {
+        if (userHome == null)
+            userHome = System.getProperty("user.home");
+        return userHome;
+    }
+
+    private static OS findOS() {
+        String osName = System.getProperty("os.name");
+
+        osName = osName.toLowerCase();
+
+        if (osName.contains("win"))
+            return OS.WINDOWS;
+        if (osName.contains("mac"))
+            return OS.OSX;
+        if (osName.contains("linux"))
+            return OS.LINUX;
+        return null;
+    }
+}
--- src/main/java/personal/mstall/main/SaveManager.java
+++ src/main/java/personal/mstall/main/SaveManager.java
@@ -1,4 +1,5 @@
 package personal.mstall.main;
+
 import java.io.File;
 
 import jakarta.xml.bind.JAXBContext;
@@ -7,10 +8,10 @@ import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 
 public class SaveManager {
-    private static final String PATH = "%s.xml";
 
     public static final Object Load(FileType type) {
-        File file = new File(String.format(PATH, type.filename));
+        File file = findFile(type.filename);
+
         if (!file.exists())
             return null;
         try {
@@ -25,20 +26,62 @@ public class SaveManager {
     }
 
     public static final boolean Save(Object object, FileType type) {
-        File file = new File(String.format(PATH, type.filename));
-        
+        File file = findFile(type.filename);
+
         if (object.getClass() != type.clss)
             return false;
 
         try {
             JAXBContext jaxbContext = JAXBContext.newInstance(type.clss);
             Marshaller marshaller = jaxbContext.createMarshaller();
-    
+
             marshaller.marshal(object, file);
             return true;
         } catch (JAXBException e) {
             e.printStackTrace();
             return false;
         }
+    }
+
+    private static final String WINDOWS_BASE_DIR = "%APPDATA%/";
+    // TODO: NOT TESTED
+    private static final String OSX_BASE_DIR = "/Library/ApplicationSupport/";
+    // TODO: NOT TESTED
+    private static final String LINUX_BASE_DIR = "/.config/";
+    // TODO: TESTED ON:
+    // POP_OS!
+    private static final String APPNAME = "Strategist/";
+    // TODO: WORKING TITLE
+    // Other ideas:
+    // The Owl Parliament
+    //
+
+    private static final String EXTENSION = ".xml";
+
+    private static final File findFile(String filename) {
+        OSUtils.OS os = OSUtils.getOS();
+
+        String dirPath = null;
+
+        switch (os) {
+            case WINDOWS:
+                dirPath = WINDOWS_BASE_DIR + APPNAME;
+                dirPath.replaceAll("/", "\\");
+                break;
+            case OSX:
+                dirPath = OSUtils.getUserHome() + OSX_BASE_DIR + APPNAME;
+                break;
+            case LINUX:
+                dirPath = OSUtils.getUserHome() + LINUX_BASE_DIR + APPNAME.toLowerCase();
+                break;
+        }
+
+        File dir = new File(dirPath);
+        if (!dir.exists())
+            dir.mkdirs();
+
+        String path = dirPath + filename + EXTENSION;
+
+        return new File(path);
     }
 }
